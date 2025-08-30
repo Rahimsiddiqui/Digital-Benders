95320780ddf676128aa133fb54cd6ae317887885
Rahim Siddiqui <rahimsiddiqui122@gmail.com>
2025-08-30 20:34:24 +0500

    .

---
+++ public/javascript/landing-page/services-card.js
@@ -0,0 +1,30 @@
+const cards = document.querySelectorAll(".services__card");
+
+cards.forEach((card) => {
+  const cardChanger = card.querySelector(".card__changer");
+
+  if (!cardChanger) return;
+
+  // MOBILE / CLICK
+  cardChanger.addEventListener("click", () => {
+    card.classList.toggle("active");
+    console.log(mainImage);
+  });
+
+  // DESKTOP / HOVER
+  cardChanger.addEventListener("mouseenter", () => {
+    card.classList.add("active");
+  });
+  card.addEventListener("mouseleave", () => {
+    card.classList.remove("active");
+  });
+});
+
+// CLICK OUTSIDE TO CLOSE ON MOBILE
+document.addEventListener("click", (e) => {
+  cards.forEach((card) => {
+    if (!card.contains(e.target)) {
+      card.classList.remove("active");
+    }
+  });
+});
