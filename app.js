3cbc65a0fa06aa16d372fdea7921b4f0aeed639c
Rahim Siddiqui <rahimsiddiqui122@gmail.com>
2025-09-17 19:30:09 +0500

    .

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-const { express, app, path, ejsMate } = require(`./dependecies`);
+const { express, app, path, ejsMate, fs } = require(`./dependecies`);
 
 app.set(`views`, path.join(__dirname, `views`));
 app.set(`view engine`, `ejs`);
+
+function loadJSON(file) {
+  return JSON.parse(
+    fs.readFileSync(path.join(__dirname, "data", file), "utf-8")
+  );
+}
 
 app.use((req, res, next) => {
   res.locals.currentRoute = req.path;
@@ -12,8 +18,20 @@ app.use(express.static(path.join(__dirname, `public`)));
 
 app.engine(`ejs`, ejsMate);
 
-app.get(`/`, (req, res) => {
-  res.render(`pages/landing-page`);
+app.get("/", (req, res) => {
+  const brandGrowing = loadJSON("brandGrowing.json");
+  const services = loadJSON("services.json");
+  const caseStudies = loadJSON("caseStudies.json");
+  const results = loadJSON("results.json");
+  const testimonials = loadJSON("testimonials.json");
+
+  res.render("pages/landing-page", {
+    caseStudies,
+    services,
+    results,
+    testimonials,
+    brandGrowing,
+  });
 });
 
 app.listen(3000, (req, res) => {
--- dependecies.js
+++ dependecies.js
@@ -2,10 +2,12 @@ const express = require(`express`);
 const app = express();
 const path = require(`path`);
 const ejsMate = require(`ejs-mate`);
+const fs = require(`fs`);
 
 module.exports = {
   express,
   app,
   path,
   ejsMate,
+  fs,
 };
