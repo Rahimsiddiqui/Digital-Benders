f9c3a3ee0d4347a006e3ae98290234c8eed12459
Rahim Siddiqui <rahimsiddiqui122@gmail.com>
2025-09-17 17:14:26 +0500

    .

--- public/javascript/landing-page/swiper.js
+++ public/javascript/landing-page/swiper.js
@@ -1,21 +1,45 @@
-new Swiper(".swiper", {
-  slidesPerView: 1,
-  spaceBetween: 10,
-  freeMode: {
-    enabled: true,
-    momentum: true,
-    momentumRatio: 1,
-  },
-  grabCursor: true,
-  autoplay: {
-    delay: 1750,
-    disableOnInteraction: false,
-  },
-  breakpoints: {
-    480: { slidesPerView: 2 },
-    778: { slidesPerView: 3 },
-    1200: { slidesPerView: 5 },
-  },
-  speed: 500,
-  loop: true,
+document.addEventListener("DOMContentLoaded", function () {
+  // Styles For Hero Swiper
+  new Swiper(".hero__swiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    grabCursor: true,
+    autoplay: {
+      delay: 1750,
+      disableOnInteraction: false,
+    },
+    speed: 500,
+    loop: true,
+    breakpoints: {
+      480: { slidesPerView: 2 },
+      778: { slidesPerView: 3 },
+      1200: { slidesPerView: 5 },
+    },
+  });
+
+  // Style For Case Studes Swiper
+  new Swiper(".case-studies__swiper", {
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 17,
+    grabCursor: true,
+    autoplay: {
+      delay: 1750,
+      disableOnInteraction: false,
+    },
+    speed: 500,
+    loop: true,
+    breakpoints: {
+      768: {
+        centeredSlides: false,
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      1024: {
+        centeredSlides: false,
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+    },
+  });
 });
