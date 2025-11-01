98c3fb6d0e9199a8a1d5c3395c44bb492d5b9452
Rahim Siddiqui <rahimsiddiqui122@gmail.com>
2025-11-01 15:50:22 +0500

    .

--- public/javascript/admin/admin-dashboard.js
+++ public/javascript/admin/admin-dashboard.js
@@ -1,22 +1,32 @@
 // Needed Elements
-const dangerBtn = document.querySelector(".logoutBtn");
+const dangerBtn = document.querySelector(".logout-icon");
 const sidebarLogo = document.querySelector(".sidebar-logo");
 const sidebar = document.querySelector(".sidebar");
 const settingsIcon = document.querySelector(".settings-icon");
 const themeIcon = document.querySelector(".theme-icon");
 const linksContainer = document.querySelector(".links");
-const links = document.querySelectorAll(".underline");
+const navLinks = document.querySelectorAll(".link");
 
 // State
 let sidebarOpen = false;
 let settingsIconRotated = false;
 let theme = "light";
 
+// FUNCTIONS
+
+const initSidebar = function (
+  sidebarState,
+  afterSidebarClosed,
+  beforeSidebarClosed
+) {
+  return sidebarState ? afterSidebarClosed : beforeSidebarClosed;
+};
+
 // --- NAV LINK ACTIVE HANDLER ---
-links.forEach((link) => {
-  link.addEventListener("click", () => {
-    document.querySelector(".underline.active")?.classList.remove("active");
-    link.classList.add("active");
+navLinks.forEach((item) => {
+  item.addEventListener("click", () => {
+    document.querySelector(".link.active")?.classList.remove("active");
+    item.classList.add("active");
   });
 });
 
@@ -29,13 +39,15 @@ themeIcon.addEventListener("click", () => {
 
     themeIcon.src = isLight
       ? "/icons/darkmode - dark.png"
-      : "/icons/lightmode - light.png";
-    themeIcon.classList.toggle("change-margin");
+      : "/icons/lightmode - dark.png";
+
     document.documentElement.setAttribute(
       "data-theme",
       isLight ? "dark" : "light"
     );
     theme = isLight ? "dark" : "light";
+
+    themeIcon.classList.toggle("changeMargin");
 
     themeIcon.classList.remove("fade");
   }, 200);
@@ -53,11 +65,22 @@ settingsIcon.addEventListener("click", () => {
 sidebarLogo.addEventListener("click", () => {
   sidebarOpen = !sidebarOpen;
 
-  linksContainer.style.opacity = sidebarOpen ? "1" : "0";
-  linksContainer.style.pointerEvents = sidebarOpen ? "auto" : "none";
-  sidebar.style.maxWidth = sidebarOpen ? "190px" : "57.4px";
-  settingsIcon.style.left = sidebarOpen ? "83px" : "18px";
-  themeIcon.style.left = sidebarOpen ? "58px" : "-8px";
+  const linkRows = document.querySelectorAll(".link__row");
+
+  linkRows.forEach((row) => {
+    row.addEventListener("click", () => {
+      // remove active from previous row
+      document.querySelector(".link__row.active")?.classList.remove("active");
+      // add active to clicked
+      row.classList.add("active");
+    });
+  });
+
+  sidebar.classList.toggle("open", sidebarOpen);
+  sidebar.style.maxWidth = initSidebar(sidebarOpen, "190px", "57.4px");
+  settingsIcon.style.left = initSidebar(sidebarOpen, "83px", "18px");
+  themeIcon.style.left = initSidebar(sidebarOpen, "77px", "12px");
+  dangerBtn.style.left = initSidebar(sidebarOpen, "78px", "13px");
 });
 
 // --- LOGOUT CONFIRMATION ---
