9a46ad001fde5f578cb2a6e21c5e221caedfb3d1
Rahim Siddiqui <rahimsiddiqui122@gmail.com>
2025-11-17 21:04:07 +0500

    .

--- routes/production-service.js
+++ routes/production-service.js
@@ -3,8 +3,9 @@ const router = express.Router();
 
 router.get("/production-service-in-canada", (_, res) => {
   const data = loadJSON("production-service/data.json");
+  const faqs = loadJSON("production-service/faqs.json");
 
-  res.render("pages/production-service", { data });
+  res.render("pages/production-service", { data, faqs });
 });
 
 module.exports = router;
